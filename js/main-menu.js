9e8251e165637dc950908f70dd8e4b5296b0fa13
kovalenkovpu <t085en@gmail.com>
2016-12-01 01:24:10 +0300

    Внесены изменения в стили всех страниц

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,1 +1,12 @@
 "use strict";
+
+var toggle = document.getElementsByClassName("main-menu__toggle")[0];
+var main_menu = document.getElementsByClassName("main-menu main-menu--visible")[0];
+var menu_items = document.getElementsByClassName("main-menu-items")[0];
+
+toggle.onclick = function() {
+  main_menu.className = "main-menu";
+  menu_items.style.display = "none";
+  toggle.style.left = "104px";
+  toggle.style.top = "30px";
+};
