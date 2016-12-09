5a8ed6f4cc16472c2104b332272d055a3b2aef32
kovalenkovpu <t085en@gmail.com>
2016-12-09 17:42:13 +0300

    Исправления по замечаниям: сетка, js, формы, блок контактов, блок товар недели

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,21 +1,29 @@
 "use strict";
 
-var toggle_wrapper = document.getElementsByClassName("main-menu__toggle-wrapper")[0];
+var toggleWrapper = document.getElementsByClassName("main-menu__toggle-wrapper")[0];
 var toggle = document.getElementsByClassName("main-menu__toggle")[0];
-var main_menu = document.getElementsByClassName("main-menu")[0];
-var menu_list = document.getElementsByClassName("main-menu__list")[0];
+var menu = document.getElementsByClassName("main-menu")[0];
+var menuList = menu.getElementsByClassName("main-menu__list")[0];
+var menuHeight = getComputedStyle(menu).height;
 
+function showMenu() {
+  menuList.classList.remove("main-menu__list--invisible");
+  menuList.classList.add("main-menu__list--visible");
+  toggle.classList.add("main-menu__toggle--crossed");
+  menu.style.height = menuHeight;
+};
 
-toggle_wrapper.onclick = function() {
-  if (menu_list.className == "main-menu__list main-menu__list--visible") {
-    menu_list.classList.remove("main-menu__list--visible");
-    menu_list.classList.add("main-menu__list--invisible");
-    toggle.classList.remove("main-menu__toggle--crossed");
-    main_menu.style.height = "0";
-  } else if (menu_list.className == "main-menu__list main-menu__list--invisible") {
-    menu_list.classList.remove("main-menu__list--invisible");
-    menu_list.classList.add("main-menu__list--visible");
-    toggle.classList.add("main-menu__toggle--crossed");
-    main_menu.style.height = "320px";
-  }
-}
+function hideMenu() {
+  menuList.classList.remove("main-menu__list--visible");
+  menuList.classList.add("main-menu__list--invisible");
+  toggle.classList.remove("main-menu__toggle--crossed");
+  menu.style.height = "0";
+};
+
+toggleWrapper.addEventListener('click', function(){
+  if (menuList.classList.contains("main-menu__list--visible")) {
+    hideMenu();
+  } else {
+    showMenu();
+  };
+});
